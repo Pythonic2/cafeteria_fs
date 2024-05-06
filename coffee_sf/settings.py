14b9dac762f963e899117fa52a602da1bee03a89
Pythonic2 <igoormarinhosilva@gmail.com>
2024-05-05 23:17:41 -0300

    projeto finalizado

--- coffee_sf/settings.py
+++ coffee_sf/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yi3(0)#5eo9mkx9af3z^hs%98@s^b9et7apu2#aqyuj1ufi*wn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
